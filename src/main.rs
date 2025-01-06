ca902d6853d7d713198f0123de4bb3f6b853c34f
Eliah Kagan <degeneracypressure@gmail.com>
2025-01-06 17:56:00 -0500

    Use `find::Never` instead of `repo.objects.clone()`
    I'm not sure which is better for this demonstration. Whichever one
    is not used should be mentioned in a comment.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     gix::worktree::state::checkout(
         &mut index,
         repo.work_dir().ok_or("need non-bare repo")?,
-        repo.objects.clone(),
+        gix_object::find::Never, // Can also use: repo.objects.clone()
         &gix::progress::Discard,
         &gix::progress::Discard,
         &gix::interrupt::IS_INTERRUPTED,
